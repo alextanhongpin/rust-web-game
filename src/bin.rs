11662ad2647a674b167009f488b63a75cd974c08
alextanhongpin <alextan@seekasia.com>
2018-02-19 20:33:10 +0800

    chore: add sample import module

--- src/bin.rs
+++ src/bin.rs
@@ -3,19 +3,21 @@
 
 #[macro_use]
 extern crate lazy_static;
+extern crate rand;
 extern crate rocket;
 extern crate rocket_contrib;
-extern crate rand;
 // extern crate time;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
+pub mod models;
+
 use rand::Rng;
 
-use rocket::request::{Outcome, Form, FromRequest, Request, FlashMessage};
+use rocket::request::{FlashMessage, Form, FromRequest, Outcome, Request};
 use rocket::Outcome::Success;
-use rocket::response::{Flash, Redirect, NamedFile};
+use rocket::response::{Flash, NamedFile, Redirect};
 use rocket::http::{Cookie, Cookies, RawStr};
 use rocket_contrib::Template;
 
@@ -35,37 +37,39 @@ pub struct Character {
     name: String,
     strength: u8,
     dexterity: u8,
-    hitpoints: u8
+    hitpoints: u8,
 }
 
 #[derive(FromForm, Debug)]
 pub struct CharacterForm {
-    name: String
+    name: String,
 }
 
 type Characters = Arc<Mutex<HashMap<String, Character>>>;
 struct GameState {
-    players: Characters
+    players: Characters,
 }
 
-impl <'a, 'r> FromRequest<'a, 'r> for GameState {
+impl<'a, 'r> FromRequest<'a, 'r> for GameState {
     type Error = std::fmt::Error;
     fn from_request(_: &'a Request<'r>) -> Outcome<Self, Self::Error> {
-        Success(GameState{ players: CHARACTERS.clone() })
+        Success(GameState {
+            players: CHARACTERS.clone(),
+        })
     }
 }
 
 #[derive(Serialize)]
 struct GameViewData {
     flash: String,
-    characters: HashMap<String, Character>
+    characters: HashMap<String, Character>,
 }
 
 #[get("/")]
 fn index(cookies: Cookies) -> Redirect {
     match cookies.get("character_id") {
         Some(_) => Redirect::to("/game"),
-        None => Redirect::to("/new")
+        None => Redirect::to("/new"),
     }
 }
 
@@ -75,18 +79,25 @@ fn new() -> Template {
 }
 
 #[post("/new", data = "<name>")]
-fn post_new(mut cookies: Cookies, name: Form<CharacterForm>, state: GameState) -> Result<Redirect, String> {
+fn post_new(
+    mut cookies: Cookies,
+    name: Form<CharacterForm>,
+    state: GameState,
+) -> Result<Redirect, String> {
     let character = name.get();
     let mut rng = rand::thread_rng();
     let new_character_id: String = rng.gen::<u64>().to_string();
 
     let ref mut players = *state.players.lock().unwrap();
-    players.insert(new_character_id.clone(), Character{
-        name: character.name.clone(),
-        strength: rng.gen::<u8>(),
-        dexterity: rng.gen::<u8>(),
-        hitpoints: rng.gen::<u8>(),
-    });
+    players.insert(
+        new_character_id.clone(),
+        Character {
+            name: character.name.clone(),
+            strength: rng.gen::<u8>(),
+            dexterity: rng.gen::<u8>(),
+            hitpoints: rng.gen::<u8>(),
+        },
+    );
     // let cookie = Cookie::build("character_id", new_character_id)
     // .path("/")
     // .secure(true)
@@ -113,13 +124,16 @@ fn attack(cookies: Cookies, state: GameState, id: &RawStr) -> Flash<Redirect> {
         format!("{} missed {}", attacker.name, defender.name)
     } else if defender.hitpoints as i16 - damage < 1 {
         players.remove(id.as_str());
-        format!("{} hits {}. {} is slain!", attacker.name, defender.name, defender.name)
+        format!(
+            "{} hits {}. {} is slain!",
+            attacker.name, defender.name, defender.name
+        )
     } else {
         let new_defender = Character {
             name: defender.name.clone(),
             strength: defender.strength,
             dexterity: defender.dexterity,
-            hitpoints: defender.hitpoints - damage as u8
+            hitpoints: defender.hitpoints - damage as u8,
         };
         players.insert(id.as_str().to_string(), new_defender);
         format!("{} hits {}", attacker.name, defender.name)
@@ -134,19 +148,26 @@ fn game(state: GameState, flash: Option<FlashMessage>) -> Template {
     let characters = players.lock().unwrap();
     let flash: String = match flash {
         Some(f) => f.msg().into(),
-        None => "".into()
+        None => "".into(),
     };
 
-    let gvd = GameViewData { flash: flash, characters: characters.clone() };
+    let gvd = GameViewData {
+        flash: flash,
+        characters: characters.clone(),
+    };
     Template::render("game", &gvd)
 }
 
-#[get("/<path..>", rank=1)]
+#[get("/<path..>", rank = 1)]
 fn static_files(path: PathBuf) -> io::Result<NamedFile> {
     NamedFile::open(Path::new("static/").join(path))
 }
 
 fn main() {
+    let post = models::Post {
+        id: "hello".to_string(),
+    };
+    println!("{:?}", post);
     rocket::ignite().
         mount("/", routes![index, new, post_new, game, attack, static_files]).
         // attach(Template::custom(|engines| {
